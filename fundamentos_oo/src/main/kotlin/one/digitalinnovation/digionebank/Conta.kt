776d9b50ba7c8e3f416583aee9a496143d734b8c
Lucas Santos <lucastsantos1020@gmail.com>
2022-06-14 13:35:16 -0300

    created Conta class

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,21 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia:String,
+    val numero:String,
+    val saldo:BigDecimal,
+        ) {
+
+    fun deposito(valor:BigDecimal){
+
+    }
+
+    fun saque (valor: BigDecimal){
+
+
+
+
+
+}
